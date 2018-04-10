1d64f077e51e9f4dfa70fb1759cb771b843b0b34
tanmay.gujar <tanmay.gujar@NIT-LPT-R027>
2018-04-10 12:51:24 +0530

    Add project files.

---
+++ MVC_Sample_App/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_Sample_App.Startup))]
+namespace MVC_Sample_App
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
